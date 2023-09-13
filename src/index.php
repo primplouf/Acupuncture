81fbdc5938350ac034fe10f523bda02ca58ed1b1
rtranvanba <rtranvanba@cirilgroup.com>
2023-09-13 17:14:02 +0200

    premier ajout html et changer le port

--- src/index.php
+++ src/index.php
@@ -1,5 +1,26 @@
 <?php
+include 'vendor/autoload.php';
 
-//PHP Router
-echo "It WORKS !"
-?>
+try {
+    // le dossier ou on trouve les templates
+    $loader = new Twig\Loader\FilesystemLoader('view');
+
+    // initialiser l'environement Twig
+    $twig = new Twig\Environment($loader);
+
+    // load template
+    $template = $twig->load('accueil.html');
+
+    // set template variables
+    // render template
+    echo $template->render(array(
+        'lundi' => 'Steak Frites',
+        'mardi' => 'Raviolis',
+        'mercredi' => 'Pot au Feu',
+        'jeudi' => 'Couscous',
+        'vendredi' => 'Poisson',
+    ));
+
+} catch (Exception $e) {
+    die ('ERROR: ' . $e->getMessage());
+}
