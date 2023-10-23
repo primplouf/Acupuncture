0c205d3b4ca2b60cb1863cd3fb6f741bc473d2bc
remi <remitvb@gmail.com>
2023-10-23 22:06:15 +0200

    ajout des templates twig

--- src/index.php
+++ src/index.php
@@ -2,31 +2,51 @@
 include 'vendor/autoload.php';
 include 'router/Route.class.php';
 
-/*
+
+
 try {
     // le dossier ou on trouve les templates
-    $loader = new Twig\Loader\FilesystemLoader('view');
+    $loader = new Twig\Loader\FilesystemLoader('views');
 
     // initialiser l'environement Twig
     $twig = new Twig\Environment($loader);
 
     // load template
-    $template = $twig->load('accueil.html');
-
-    // set template variables
-    // render template
-    echo $template->render(array(
-        'lundi' => 'Steak Frites',
-        'mardi' => 'Raviolis',
-        'mercredi' => 'Pot au Feu',
-        'jeudi' => 'Couscous',
-        'vendredi' => 'Poisson',
-    ));
-
+    if(isset($_GET["page"])) {
+        $page = htmlspecialchars($_GET["page"]);
+        switch ($page) {
+            case 'accueil':
+                echo $twig->render('accueil.twig');
+                break;
+            case 'recherchePathologie':
+                echo $twig->render('recherchePathologie.twig');
+                break;
+            case 'filtrePathologie':
+                echo $twig->render('filtrePathologie.twig');
+                break;
+            case 'keywords':
+                echo $twig->render('keywords.twig');
+                break;
+            case 'inscription':
+                echo $twig->render('inscription.twig');
+                break;
+            case 'connexion':
+                echo $twig->render('connexion.twig');
+                break;
+            
+            default:
+                header('HTTP/1.0 404 Not Found');
+                echo $twig->render('404.twig');
+                break;
+        }
+    } else {
+        echo $twig->render('accueil.twig');
+    }
+    
 } catch (Exception $e) {
     die ('ERROR: ' . $e->getMessage());
 }
-*/
+
 $routes = [];
 
 $controllers = array_diff(scandir("controllers/"), array('..', '.'));
