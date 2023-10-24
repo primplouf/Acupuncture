fb212f3989ffad33df3d4df755e9dd30b9496e0f
primplouf <primplouf@LAPTOP-0AR34JSI.localdomain>
2023-10-24 09:39:44 +0200

    add login and register

--- src/controllers/SessionController.php
+++ src/controllers/SessionController.php
@@ -7,26 +7,27 @@ require_once('./models/User.class.php');
 #[Prefix('/user')]
 class SessionController {
 
+    private $_bdd;
     private $_userManager;
 
     public function __construct(){
-        $_userManager = new UserManager((new Database())->connectDb());
+        $this->_bdd = (new Database())->connectDb();
+        $this->_userManager = new UserManager($this->_bdd);
     }
 
     #[Route('/register','POST','register')]
     public function register(){
 
-        if (isset($_POST["surname"]) && isset($_POST["nom"]) && isset($_POST["email"]) && isset($_POST["mdp"])) {
-
+        if (isset($_POST["firstname"]) && isset($_POST["lastname"]) && isset($_POST["email"]) && isset($_POST["pwd"])) {
+            
             $firstname = $_POST["firstname"];
             $lastname = $_POST["lastname"];
             $email = $_POST["email"];
             $pwd = $_POST["pwd"];
-            $hash = password_hash($pwd, PASSWORD_DEFAULT);
-            $user = new User(array("firstname" => $firstname, "lastname" => $lastname, "email" => $email, "pwd" => $hash));
-
+            $user = new User(array("firstname" => $firstname, "lastname" => $lastname, "email" => $email, "pwd" => $pwd));
+            
             if ($this->_userManager->noRegister($user)) {
-
+                echo "noregistre";
                 $res = $this->_userManager->addUser($user);
                 if ($res){
                     echo '<p style="color:green;">Inscription effectué avec succès vous pouvez vous connecter</p>';
@@ -38,6 +39,26 @@ class SessionController {
             }
         }
 
+    }
+
+    #[Route("/login", "GET", "login")]
+    public function login(){
+
+        if (isset($_POST['email']) and !empty($_POST['email']) and isset($_POST['pwd']) and !empty($_POST['pwd'])) {
+
+            $email = htmlspecialchars($_POST['email']);
+            $pwd = htmlspecialchars($_POST['pwd']);
+            $new_user = new User(array('email' => $email));
+            $isConnected = $this->_userManager->checkConnection($new_user, $pwd);
+
+            if ($isConnected) {
+                session_start();
+                $_SESSION['email'] = $_POST['email'];
+                
+                header('Location: index.php?page=keywords');
+                exit();
+            }
+        }
     }
 }
 
